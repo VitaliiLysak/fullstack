f8e3e52694d817c432f7cd1cade53c9a074bc077
dogfox <dogfox.ga@gmail.com>
2018-08-25 21:19:33 +0300

    'Hmm....'

--- routes/position.js
+++ routes/position.js
@@ -2,6 +2,7 @@ const express = require('express')
 const controller = require('../controllers/position')
 const router = express.Router()
 
+//Hm...
 router.get('/:categoryId', controller.getByCategoryId)
 router.post('/', controller.create)
 router.patch('/:id', controller.update)
